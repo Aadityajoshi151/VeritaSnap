b9d05b6e2629e03b711f23c2c6a0c859a5c07f5c
aadityajoshi151 <aadityajoshi151@gmail.com>
2021-08-22 11:00:40 +0530

    inital setup

---
+++ main.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow } = require('electron')
+let win
+
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+    },
+  });
+
+  // and load the index.html of the app.
+  win.loadFile('index.html')
+
+  // Open the DevTools.
+  //win.webContents.openDevTools()
+
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
